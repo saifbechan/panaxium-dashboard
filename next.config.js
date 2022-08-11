995c82c39b753b57297d1e214a3312a4c77c8148
Saif Bechan <saifbechan@gmail.com>
2022-08-12 00:53:27 +0200

    feat: implemented PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,29 @@ import '@fontsource/open-sans/700.css';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import theme from '../styles/theme';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <RecoilRoot>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </RecoilRoot>
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </ChakraProvider>
+    </>
   );
 }
 
