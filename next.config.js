d3a8a0c911891762f811e2b5d5b40f6356c39c04
Saif Bechan <saifbechan@gmail.com>
2022-08-12 00:58:13 +0200

    fix: updated next config

--- next.config.js
+++ next.config.js
@@ -4,11 +4,21 @@ const withPWA = require('next-pwa');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = withPWA({
-  ...nextConfig,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
   pwa: {
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
   },
-});
+};
+
+module.exports = () => {
+  const plugins = [withPWA];
+  const config = plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig,
+  });
+  return config;
+};
