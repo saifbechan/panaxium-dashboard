c6377973b09a101da918838f937faa55b25b1564
Saif Bechan <saifbechan@gmail.com>
2022-08-12 16:48:59 +0200

    feat: keep state in between renders

--- components/RawSignals/index.tsx
+++ components/RawSignals/index.tsx
@@ -2,16 +2,19 @@ import 'chartjs-adapter-luxon';
 import { Box, Tooltip as CTooltip, Heading, Icon } from '@chakra-ui/react';
 import { BsQuestionCircle } from 'react-icons/bs';
 import {
+  BubbleDataPoint,
   CategoryScale,
   Chart as ChartJS,
   Legend,
   LineElement,
   LinearScale,
   PointElement,
+  ScatterDataPoint,
   Title,
   Tooltip,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import { useRef } from 'react';
 import StreamingPlugin from 'chartjs-plugin-streaming';
 import zoomPlugin from 'chartjs-plugin-zoom';
 
@@ -28,6 +31,8 @@ ChartJS.register(
 );
 
 const RawSignals = () => {
+  const datasets = useRef<(number | ScatterDataPoint | BubbleDataPoint | null)[][]>([[], []]);
+
   return (
     <Box backgroundColor="#29293B" padding="10px">
       <Box textAlign="right">
@@ -51,7 +56,7 @@ const RawSignals = () => {
               borderColor: '#59486A',
               cubicInterpolationMode: 'monotone',
               fill: true,
-              data: [],
+              data: datasets.current[0],
             },
             {
               label: 'Dataset 2',
@@ -59,7 +64,7 @@ const RawSignals = () => {
               borderColor: '#401D56',
               cubicInterpolationMode: 'monotone',
               fill: true,
-              data: [],
+              data: datasets.current[1],
             },
           ],
         }}
@@ -87,11 +92,13 @@ const RawSignals = () => {
               realtime: {
                 delay: 2000,
                 onRefresh: (chart) => {
-                  chart.data.datasets.forEach((dataset) => {
+                  chart.data.datasets.forEach((dataset, index) => {
                     dataset.data.push({
                       x: Date.now(),
                       y: Math.random(),
                     });
+
+                    datasets.current[index] = dataset.data;
                   });
                 },
               },
