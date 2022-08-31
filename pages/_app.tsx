ecd0590d296c18ee83e560d018dc1760994aeff4
Saif Bechan <saifbechan@gmail.com>
2022-08-31 14:22:26 +0200

    feat: better icons for PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
           name="viewport"
         />
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
         <link href="/manifest.json" rel="manifest" />
         <title>Panaxium EEG Dashboard</title>
       </Head>
