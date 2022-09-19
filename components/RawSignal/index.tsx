87ba4c9b073e85770d02473217af0d45a4d0a314
Saif Bechan <saifbechan@gmail.com>
2022-09-19 16:50:11 +0200

    chore: removed chart refs

--- components/RawSignal/index.tsx
+++ components/RawSignal/index.tsx
@@ -4,7 +4,7 @@ import { Chart } from 'react-chartjs-2';
 import { Text } from '@chakra-ui/react';
 import { selectedSignalState, signalsMinMaxState } from '../../lib/store';
 import { useAtom, useAtomValue } from 'jotai';
-import { useMemo, useRef } from 'react';
+import { useRef } from 'react';
 import Section from '../Section';
 
 const RawSignal = ({
@@ -16,9 +16,9 @@ const RawSignal = ({
   signal: number;
   lfpSegment: number[][];
 }) => {
-  const chartRef = useRef<ChartJS>(null);
   const [selectedSignal, setSelectedSignal] = useAtom(selectedSignalState);
   const { min, max } = useAtomValue(signalsMinMaxState);
+
   const pointer = useRef(0);
 
   return (
@@ -26,61 +26,54 @@ const RawSignal = ({
       <Text fontSize="xs" mb={1}>
         {signal}
       </Text>
-      {useMemo(
-        () => (
-          <Chart
-            ref={chartRef}
-            data={{
-              datasets,
-            }}
-            options={{
-              aspectRatio: 0.75,
-              events: ['click'],
-              onClick: () => {
-                setSelectedSignal(signal);
-              },
-              plugins: {
-                legend: { display: false },
-                datalabels: { display: false },
-              },
-              scales: {
-                x: {
-                  display: false,
-                  type: 'realtime',
-                  realtime: {
-                    delay: 2000,
-                    refresh: 20,
-                    onRefresh: (chart: ChartJS) => {
-                      chart.data.datasets.forEach((dataset) => {
-                        const next = {
-                          x: Date.now(),
-                          y: lfpSegment[signal - 1][pointer.current],
-                        };
-                        dataset.data.push(next);
-                      });
+      <Chart
+        data={{
+          datasets,
+        }}
+        options={{
+          aspectRatio: 0.75,
+          events: ['click'],
+          onClick: () => {
+            setSelectedSignal(signal);
+          },
+          plugins: {
+            legend: { display: false },
+            datalabels: { display: false },
+          },
+          scales: {
+            x: {
+              display: false,
+              type: 'realtime',
+              realtime: {
+                delay: 2000,
+                refresh: 20,
+                onRefresh: (chart: ChartJS) => {
+                  chart.data.datasets.forEach((dataset) => {
+                    const next = {
+                      x: Date.now(),
+                      y: lfpSegment[signal - 1][pointer.current],
+                    };
+                    dataset.data.push(next);
+                  });
 
-                      if (pointer.current === 500) {
-                        pointer.current = 0;
-                        return;
-                      }
+                  if (pointer.current === 500) {
+                    pointer.current = 0;
+                    return;
+                  }
 
-                      pointer.current += 1;
-                    },
-                  },
-                },
-                y: {
-                  display: false,
-                  min,
-                  max,
+                  pointer.current += 1;
                 },
               },
-            }}
-            type="line"
-          />
-        ),
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        []
-      )}
+            },
+            y: {
+              display: false,
+              min,
+              max,
+            },
+          },
+        }}
+        type="line"
+      />
     </Section>
   );
 };
--- components/RhythmAll/index.tsx
+++ components/RhythmAll/index.tsx
@@ -5,7 +5,7 @@ import { Collapse } from '@chakra-ui/react';
 import { faker } from '@faker-js/faker';
 import { sectionTogglesState, signalsMinMaxState } from '../../lib/store';
 import { useAtomValue } from 'jotai';
-import { useMemo, useRef } from 'react';
+import { useRef } from 'react';
 import Section from '../Section';
 
 const RhythmAll = ({
@@ -16,67 +16,60 @@ const RhythmAll = ({
   lfpSegment: number[][];
 }) => {
   const sectionToggles = useAtomValue(sectionTogglesState);
-  const chartRef = useRef<ChartJS>(null);
   const { min, max } = useAtomValue(signalsMinMaxState);
+
   const pointer = useRef(0);
 
   return (
     <Collapse animateOpacity in={sectionToggles['rhythm_all']} style={{ gridColumn: 'span 2' }}>
       <Section border="1px solid #401D56" info="Some extra information" title="Rhythm (all)">
-        {useMemo(
-          () => (
-            <Chart
-              ref={chartRef}
-              data={{
-                datasets,
-              }}
-              options={{
-                plugins: {
-                  legend: { display: false },
-                  datalabels: { display: false },
-                },
-                scales: {
-                  x: {
-                    display: false,
-                    type: 'realtime',
-                    realtime: {
-                      delay: 2000,
-                      refresh: 20,
-                      onRefresh: (chart: ChartJS) => {
-                        chart.data.datasets.forEach((dataset, index) => {
-                          const y = lfpSegment[index]
-                            ? lfpSegment[index][pointer.current]
-                            : faker.datatype.float({ min, max });
+        <Chart
+          data={{
+            datasets,
+          }}
+          options={{
+            plugins: {
+              legend: { display: false },
+              datalabels: { display: false },
+            },
+            scales: {
+              x: {
+                display: false,
+                type: 'realtime',
+                realtime: {
+                  delay: 2000,
+                  refresh: 20,
+                  onRefresh: (chart: ChartJS) => {
+                    chart.data.datasets.forEach((dataset, index) => {
+                      const y = lfpSegment[index]
+                        ? lfpSegment[index][pointer.current]
+                        : faker.datatype.float({ min, max });
 
-                          const next = {
-                            x: Date.now(),
-                            y,
-                          };
-                          dataset.data.push(next);
-                        });
+                      const next = {
+                        x: Date.now(),
+                        y,
+                      };
+                      dataset.data.push(next);
+                    });
 
-                        if (pointer.current === 500) {
-                          pointer.current = 0;
-                          return;
-                        }
+                    if (pointer.current === 500) {
+                      pointer.current = 0;
+                      return;
+                    }
 
-                        pointer.current += 1;
-                      },
-                    },
-                  },
-                  y: {
-                    display: false,
-                    min,
-                    max,
+                    pointer.current += 1;
                   },
                 },
-              }}
-              type="line"
-            />
-          ),
-          // eslint-disable-next-line react-hooks/exhaustive-deps
-          []
-        )}
+              },
+              y: {
+                display: false,
+                min,
+                max,
+              },
+            },
+          }}
+          type="line"
+        />
       </Section>
     </Collapse>
   );
