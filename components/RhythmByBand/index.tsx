766e75ecec61263e6018d6ee14da5b5eee423cf7
Saif Bechan <saifbechan@gmail.com>
2022-10-08 16:08:36 +0200

    feat: added select box to rhythm

--- components/RhythmByBand/index.tsx
+++ components/RhythmByBand/index.tsx
@@ -1,4 +1,4 @@
-import { Collapse } from '@chakra-ui/react';
+import { Collapse, Select, Spacer } from '@chakra-ui/react';
 import { sectionTogglesState } from '../../lib/store';
 import { useAtomValue } from 'jotai';
 import DeviceConfig from '../DeviceConfig';
@@ -10,6 +10,17 @@ const RhythmByBand = () => {
   return (
     <Collapse animateOpacity in={sectionToggles['rhythm_band']}>
       <Section border="1px solid #401D56" info="Some extra information" title="Rhythm by band">
+        <Select borderColor="gray.400" color="gray.400" placeholder="Select channel" size="xs">
+          {' '}
+          {[...new Array(128)].map((_item, index) => (
+            <option key={index} value="option3">
+              Channel {index + 1}
+            </option>
+          ))}
+        </Select>
+
+        <Spacer h={5} />
+
         <DeviceConfig />
       </Section>
     </Collapse>
