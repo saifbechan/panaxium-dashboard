4a1773eb2c99b069676f1c45738ea5a8e586eed2
Saif Bechan <saifbechan@gmail.com>
2022-10-09 14:03:58 +0200

    feat: adjusted e2e test

--- e2e/Home.spec.tsx
+++ e2e/Home.spec.tsx
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('should display the welcome message', async ({ page }) => {
+test('should display the patients name', async ({ page }) => {
   await page.goto('/');
 
   await expect(page.locator('h1')).toContainText('Name: John Doe');
